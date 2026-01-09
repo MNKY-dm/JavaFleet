77c64efa38a5d795bb1186021888c2fd38b25e73
Dylan <dylan.moulin9272@gmail.com>
2026-01-09 19:50:32 +0100

    update model/type/Coordinate

--- src/model/type/Coordinate.java
+++ src/model/type/Coordinate.java
@@ -1,33 +1,19 @@
 package model.type;
 
-public class Coordinate {
-    private int x;
-    private int y;
-    // Penser à vérifier si les coordonnées sont comprises dans le plateau
+public final class Coordinate {
+    private final int x;
+    private final int y;
 
     public Coordinate(int x, int y) {
         this.x = x;
         this.y = y;
-        // Penser à vérifier si les coordonnées sont comprises dans le plateau
     }
 
-    // Getters
-    public int getX() {
-        return x;
-    }
+    public int getX() { return x; }
+    public int getY() { return y; }
 
-    public int getY() {
-        return y;
+    @Override
+    public String toString() {
+        return "(" + x + "," + y + ")";
     }
-
-    // Setters
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
-
-    // Autres fonctions plus tard
 }
