bd3cd1cf100aaf101210f98b5720458f7ba72ebf
Dylan <dylan.moulin9272@gmail.com>
2026-01-10 17:16:27 +0100

    update model/Game : update constructor

--- src/model/Game.java
+++ src/model/Game.java
@@ -9,11 +9,19 @@ public class Game {
     private GameState gameState;
     private int turn;
 
-    public  Game(Player[] players, Player currentPlayer, GameState gameState, int turn) {
-        this.players = players;
-        this.currentPlayer = currentPlayer;
-        this.gameState = gameState;
-        this.turn = turn;
+    public  Game(String player1Name, String player2Name) {
+        this.players = new Player[2];
+        this.players[0] = new Player(player1Name);
+        this.players[1] = new Player(player2Name);
+
+        // Initialiser la dualité des plateaux
+        this.players[0].setOpponentBoard(this.players[1].getMyBoard());
+        this.players[1].setOpponentBoard(this.players[0].getMyBoard());
+
+
+        this.gameState = GameState.SETUP; // Le jeu est en phase de construction
+        this.turn = 0; // Tour 0
+        this.currentPlayer = this.players[0]; // Le joueur 1 player1Name
     }
 
     public Player[] getPlayers() {
