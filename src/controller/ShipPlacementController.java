a67bd3d7d02e27224c37dba352580bb5d33cd25d
Dylan <dylan.moulin9272@gmail.com>
2026-01-12 12:15:32 +0100

    update controller/ShipPlacementController : add initializeGridPane, onShipLabelClicked, onGridCellClicked, rotateShip methods

--- src/controller/ShipPlacementController.java
+++ src/controller/ShipPlacementController.java
@@ -2,8 +2,11 @@ package controller;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.control.Label;
+import model.Board;
+import model.Cell;
 import model.Ship;
 import model.entity.Player;
 import type.Orientation;
@@ -21,6 +24,12 @@ public class ShipPlacementController implements Initializable {
     private ArrayList<Ship> ships;
 
     @FXML
+    private GridPane gridPane;
+
+    @FXML
+    private Button[][] cellsButtons = new Button[10][10];
+
+    @FXML
     private VBox shipsVbox;
 
     @FXML
@@ -32,11 +41,35 @@ public class ShipPlacementController implements Initializable {
     @FXML
     private Button rotateButton;
 
+    @FXML
+    private Label selectedShipLabel;
+
     public void initialize(URL location, ResourceBundle resources) {
         System.out.println("Initializing ShipPlacementController");
         ships = GameManager.getInstance().getGame().getCurrentPlayer().getShips();
 
         refreshShipsList();
+        initializeGridPane(gridPane, currentPlayer.getMyBoard());
+    }
+
+    private void initializeGridPane(GridPane gridPane, Board board) {
+        System.out.println("Initializing shipPlacementGridPane");
+        for (int y = 0 ; y < board.getHeight() ; y++) {
+            for (int x = 0 ; x < board.getWidth() ; x++) {
+                Cell cell = board.getCell(x, y);
+
+                Button cellButton = new Button(); // Crée un bouton
+                cellButton.setPrefSize(40, 40);
+                cellButton.setStyle("-fx-background-color: #87CEEB !important; " + "-fx-border-color: #000 !important; " + "-fx-border-width: 0.5 !important;");
+
+                cellButton.setOnAction(event -> {
+                    System.out.println("Cellule cliquée : (" + cell.getX() + ", " + cell.getY() + ")");
+                    onGridCellClicked(cell.getX(), cell.getY());
+                });
+                cellsButtons[x][y] = cellButton;
+                gridPane.add(cellButton, x, y); // Ajoute le bouton à la cellule
+            }
+        }
     }
 
     public void refreshShipsList() {
@@ -51,10 +84,45 @@ public class ShipPlacementController implements Initializable {
                 System.out.println("Bateau non-placé : " + ship.getShipType());
                 Label shipLabel = new Label(ship.getShipType());
                 System.out.println(ship.getShipType());
+                shipLabel.setOnMouseClicked(event -> {
+                    onShipLabelClicked(ship, shipLabel);
+                });
                 this.shipsVbox.getChildren().add(shipLabel);
+
             }
         }
         System.out.println("Tous les bateaux sont placés dans la VBox.");
+    }
+
+    private void onShipLabelClicked(Ship ship, Label shipLabel) {
+        if (this.selectedShipLabel == null) {
+            shipLabel.setStyle("-fx-background-color: #CCCCCC; -fx-padding: 5;");
+            selectedShipLabel = shipLabel;
+        } else {
+            selectedShipLabel.setStyle("-fx-background-color: transparent;");
+            System.out.println("Réinitialisation du style du shipLabel sélectionné précédent");
+            selectedShipLabel = shipLabel;
+            shipLabel.setStyle("-fx-background-color: #CCCCCC; -fx-padding: 5;");
+        }
+        currentShip = ship;
+        currentShipOrientation = Orientation.HORIZONTAL;
+    }
+
+    private void onGridCellClicked(int x, int y) {
+        if (currentShip != null) {
+            setShipPlacementPreview(x, y);
+        } else {
+            System.out.println("Sélectionnez un bateau d'abord");
+        }
+    }
+
+    @FXML
+    private void rotateShip() {
+        if (currentShipOrientation == Orientation.HORIZONTAL) {
+            currentShipOrientation = Orientation.VERTICAL;
+        } else {
+            currentShipOrientation = Orientation.HORIZONTAL;
+        }
     }
 
     // Getters
